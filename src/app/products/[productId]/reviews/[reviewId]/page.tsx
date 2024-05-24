4da1113fa371dabaa4b5acf4db91ec6d814b1c70
akkharadet phuangphaka <akkaradet.p@ubu.ac.th>
2024-05-24 10:55:45 +0700

    add error page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import { notFound } from "next/navigation";
+
+function getRandInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 
 export default function ProductReviewPage({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (getRandInt(2) === 1) {
+    throw new Error("Random error");
+  }
   if( parseInt(params.productId) > 10 || parseInt(params.reviewId) > 10) {
     notFound();
   }
